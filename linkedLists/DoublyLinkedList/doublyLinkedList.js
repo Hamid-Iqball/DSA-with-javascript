a16a2e41712a064b02274538c609a7a8c702875f
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2024-06-14 08:11:16 -0700

    completed doubleLinkedList

--- linkedLists/DoublyLinkedList/doublyLinkedList.js
+++ linkedLists/DoublyLinkedList/doublyLinkedList.js
@@ -38,27 +38,77 @@ class DoublyLinkedLinst {
     this.length--;
     return current;
   }
+
   shift() {
-    if (this.length === 0) return null;
-    let current = this.head;
+    if (!this.head) return undefined;
+    let oldHead = this.head;
     if (this.length === 1) {
       this.head = null;
       this.tail = null;
     } else {
-      this.head = current.next;
-      current.prev = null;
-      current.next = null;
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
     }
     this.length--;
-    return current;
+    return oldHead;
+  }
+
+  unShift(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index <= this.length / 2) {
+      let count = 0;
+      let current = this.head;
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+      return current;
+    } else {
+      let current = this.tail;
+      let count = this.length - 1;
+      while (count != index) {
+        current = current.prev;
+        count--;
+      }
+      return current;
+    }
+  }
+  set(index, value) {
+    let foundNode = this.get(index);
+    if (foundNode != null) {
+      foundNode.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === 0) {
+      this.unShift(value);
+    }
+    if ((index = this.length - 1)) {
+      this.push(value);
+    }
+    let foundNode = this.get(index - 1);
   }
 }
 
-let list = new DoublyLinkedLinst();
-list.push(1);
-list.push(2);
-list.push(3);
-list.push(4);
-list.pop();
-console.log(list.shift());
-console.log(list);
+console.log(twoSum([2, 5, 7, 11], 9));
