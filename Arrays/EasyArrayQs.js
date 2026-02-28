9ff768fd7cf5deb56a7c7b40798c1bf193fb536b
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2026-02-28 12:01:10 +0500

    Third largest element in an Array

--- Arrays/EasyArrayQs.js
+++ Arrays/EasyArrayQs.js
@@ -31,6 +31,42 @@ function secondLargestElement(arr){
     return -1
 }
 
+/*
+##########################################################################
+   Find The Third Largest Element in an Array
+##########################################################################
+*/
+
+
+function thirdlargestElement(arr){
+let n = arr.length;
+
+let first = -Infinity
+let second = -Infinity
+let third = -Infinity
+
+
+for(let i=0; i<=n; i++){
+    if(arr[i]>first){
+        third = second
+        second = first
+        first =arr[i]
+    }else if(arr[i]>second && arr[i]<first){
+ 
+        third = second
+        second  = arr[i]
+    }else if (arr[i]<second && arr[i]>third){
+        
+        third =arr[i]
+    }
+}
+return third
+
+
+}
+
+
+
 
 
 
