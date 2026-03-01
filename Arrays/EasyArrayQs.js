ca96544003a2b4f668087afd7a3a36e33b58f97e
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2026-03-02 02:32:00 +0500

    group reverse and rotate by d

--- Arrays/EasyArrayQs.js
+++ Arrays/EasyArrayQs.js
@@ -3,7 +3,7 @@
 
 /*
 ##########################################################################
-   Find The Second Largest Element in an Array
+           Find The Second Largest Element in an Array
 ##########################################################################
 */
 
@@ -33,7 +33,7 @@ function secondLargestElement(arr){
 
 /*
 ##########################################################################
-   Find The Third Largest Element in an Array
+            Find The Third Largest Element in an Array
 ##########################################################################
 */
 
@@ -66,8 +66,84 @@ return third
 }
 
 
+/*
+##########################################################################
+                     Reverse an Array (O(n), O(1))
+##########################################################################
+*/
 
 
 
+function reverseArray(arr){
+ 
+  let n = arr.length
+   for(let i=0; i<=n/2; i++){
+    [arr[i], arr[n-i-1]] = [arr[n-i-1], arr[i]]
+   }
 
+   return arr
+}
+
+
+/*
+##########################################################################
+                  Reverse an Array
+##########################################################################
+*/
+
+
+function rotateArray(arr,k){
+   
+
+    let n = arr.length
+   for(let i=0; i<n; i+=k){
+
+    let left = i
+
+    let right = Math.min(i+k-1, n-1)
+   
+    while(left<right){
+        [arr[right], arr[left]]  =[arr[left] , arr[right]]
+        left++;
+        right--
+    }
+    
+}
+    return arr
+
+
+}
+
+
+/*
+##########################################################################
+                     Reverse Array in Groups(O(n), O(1))
+##########################################################################
+*/
+
+
+function rotateArray(arr, d){
+
+ let n = arr.length;
+
+ d%n
+
+ let temp = new Array(n)
+
+ for (let i=0; i<n-d; i++){
+ temp[i] = arr[i+d]
+ }
+
+
+ for(let i=0; i<d; i++){
+    temp[i+n-d] = arr[i]
+ }
+
+ for(let i=0; i<temp.length; i++){
+    arr[i]  =temp[i]
+ }
+
+ return arr
+
+}
 
